de04983e6599739e8bbfb3db058f970fc2689aa9
yash-ak <133066101+yash-ak@users.noreply.github.com>
2023-09-11 15:12:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+
+/*********************************************************************************
+*  BTI325 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Yash A. Akabri Student ID: 126403229 Date: 09-10-23
+*
+*  Online (Cyclic) URL: https://real-rose-dog-boot.cyclic.app/
+*
+********************************************************************************/ 
+
 const app = require('./app')
 
 const port = process.env.PORT || 3000
